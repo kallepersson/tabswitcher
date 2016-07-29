615d74f237da722f2ccb4fecf6397fe6369a0657
Kalle Persson <kalle@Kalles-MacBook-Air.local>
2016-07-29 21:05:08 +0200

    Add export gif script

---
+++ export_gif.sh
@@ -0,0 +1,1 @@
+ffmpeg -y -i i.mov -vf fps=10,scale=640:-1:flags=lanczos,palettegen palette.png && ffmpeg -i i.mov -i palette.png -filter_complex "fps=10,scale=640:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
