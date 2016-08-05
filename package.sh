cfdf98fbff60b7776984d6928232e69156124c50
Kalle Persson <kalle@Kalles-MacBook-Air.local>
2016-08-06 00:23:32 +0200

    Update package script to open dev portal site after packaging
    Also saves tabswitcher.zip in current dir

--- package.sh
+++ package.sh
@@ -1,1 +1,2 @@
-zip -r ../tabswitcher.zip . -x \*.git* \*package.sh \*.DS_Store \*sources/*
+zip -r ./tabswitcher.zip . -x \*.git* \*package.sh \*.DS_Store \*sources/*;
+open https://chrome.google.com/webstore/developer/edit/gcilookdakgpccpbcjgnpaecofklimck
