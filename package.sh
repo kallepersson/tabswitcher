2b1243a0b7f28cf59ac3af67eb893976968dad09
Kalle Persson <kalle@kallepersson.se>
2018-07-21 19:46:51 +0200

    Update package script with new Extension Dashboard URL

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 zip -r ./tabswitcher.zip . -x \*.git* \*.zip* \*.sh* \*.DS_Store \*screenshot.png \*.md \*sources/*;
-open https://chrome.google.com/webstore/developer/edit/gcilookdakgpccpbcjgnpaecofklimck
+open https://chrome.google.com/webstore/devconsole/g01166460943613636556/gcilookdakgpccpbcjgnpaecofklimck/edit/package?hl=en_US
