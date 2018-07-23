71e75fe0d5f0cbea48cfbfac59acf8756a026f4b
Kalle Persson <kalle@kallepersson.se>
2018-07-23 22:42:05 +0200

    Add options page for toggling whether to display tabs from all windows (default = yes)

--- background.js
+++ background.js
@@ -1,23 +1,29 @@
 (function(){
-	var _port
-	var _updateTimeout = null
-	var _cacheThrottling = 100
+	var _updateTimeout = null;
+	var _cacheThrottling = 100;
+	var _showTabsFromAllWindows = true;
 
 	const updateTabCache = function() {
 		_updateTimeout = null;
 		var tabsCache = [];
-		chrome.windows.getAll({windowTypes:["normal"]}, function(windows) {
-			let windowTabsRemaining = windows.length;
-			windows.forEach(function(win, wi) {
-				chrome.tabs.query({windowId:win.id}, function(tabs) {
-					tabsCache = tabsCache.concat(tabs)
-					windowTabsRemaining--;
-					if (windowTabsRemaining == 0) {
-						setTabCache(tabsCache)
-					}
-				});
-			});	
-		})
+		if (_showTabsFromAllWindows) {
+			chrome.windows.getAll({windowTypes:["normal"]}, function(windows) {
+				let windowTabsRemaining = windows.length;
+				windows.forEach(function(win, wi) {
+					chrome.tabs.query({windowId:win.id}, function(tabs) {
+						tabsCache = tabsCache.concat(tabs)
+						windowTabsRemaining--;
+						if (windowTabsRemaining == 0) {
+							setTabCache(tabsCache)
+						}
+					});
+				});	
+			})
+		} else {
+			chrome.tabs.query({currentWindow:true}, function(tabs) {
+				setTabCache(tabs);
+			});
+		}
 	}
 
 	const setTabCache = function(tabCache) {
@@ -29,6 +35,23 @@
 		_updateTimeout = setTimeout(updateTabCache, _cacheThrottling);
 	}
 
+	const onStorageChanged = function(changes, areaName) {
+		if (areaName != "sync") {
+			return;
+		}
+
+		loadConfiguration(updateTabCache);
+	}
+
+	const loadConfiguration = function(callback) {
+		chrome.storage.sync.get({showTabsFromAllWindows:true}, (items) => {
+			_showTabsFromAllWindows = items.showTabsFromAllWindows;
+			if (callback) {
+				callback();
+			}
+		});
+	}
+
 	chrome.tabs.onUpdated.addListener(throttledUpdateTabCache)
 	chrome.tabs.onDetached.addListener(throttledUpdateTabCache)
 	chrome.tabs.onRemoved.addListener(throttledUpdateTabCache)
@@ -37,5 +60,7 @@
 	chrome.tabs.onCreated.addListener(throttledUpdateTabCache)
 	chrome.windows.onFocusChanged.addListener(throttledUpdateTabCache)
 
-	updateTabCache()
+	chrome.storage.onChanged.addListener(onStorageChanged)
+
+	loadConfiguration(updateTabCache);
 })()
